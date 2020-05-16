1b614f64c99d9146ef565e726d85a25213afc2cf
adisylejmani <adisylejmani123@gmail.com>
2020-05-16 22:57:36 +0200

    Changes on footer

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -4,13 +4,130 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
   <script src="<?php echo URLROOT; ?>/js/main2.js"></script>
   <!-- Futeri -->
-<footer class="page-footer font-small mdb-color pt-4">
+  <!-- Footer -->
+<footer class="page-footer font-small unique-color-dark">
+ 
+ <div style="background-color: #6351ce;">
+   <div class="container">
+
+     <!-- Grid row-->
+     <div class="row py-4 d-flex align-items-center">
+
+       <!-- Grid column -->
+       <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+         <h5 class="mb-0">Mire se vini ne Portalin Informativ te Kosoves</h5>
+       </div>
+       <!-- Grid column -->
+
+      
+       <!-- Grid column -->
+
+     </div>
+     <!-- Grid row-->
+
+   </div>
+ </div>
+
+ <!-- Footer Links -->
+ <div class="container text-center text-md-left mt-5">
+
+   <!-- Grid row -->
+   <div class="row mt-3">
+
+     <!-- Grid column -->
+     <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+       <!-- Content -->
+       <h6 class="text-uppercase font-weight-bold">Portali Informativ i Kosoves</h6>
+       <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+       <p>Kjo faqe ne vitin 2019 ishte vetem nje ide, ndersa sot eshte njera nder portalet me te sakta ne ballkan, me thjeshte eshte vetem nje KRYEVPER!</p>
+
+     </div>
+     <!-- Grid column -->
+
+     <!-- Grid column -->
+     <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+       <!-- Links -->
+       <h6 class="text-uppercase font-weight-bold">Useful links</h6>
+       <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+       <p>
+         <a href="http://fiek.uni-pr.edu">Fakulteti Teknik</a>
+       </p>
+       <p>
+         <a href="http://kompjuterika.tk/my/">Kompjuterika Moodle</a>
+       </p>
+       <p>
+         <a href="https://www.youtube.com/watch?v=j58V2vC9EPc">Greatest song</a>
+       </p>
+       <p>
+         <a href="http://localhost:8080/shareposts/pages/about">About Us</a>
+       </p>
+
+     </div>
+     <!-- Grid column -->
+
+     <!-- Grid column -->
+     <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+       <!-- Links -->
+       <h6 class="text-uppercase font-weight-bold">Creators</h6>
+       <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+       <p>
+         Adi Sylejmani
+       </p>
+       <p>
+         Art Ahemtaj
+       </p>
+       <p>
+         Rinor Ahmeti
+       </p>
+       <p>
+         Samir Simnica
+       </p>
+
+     </div>
+     <!-- Grid column -->
+
+     <!-- Grid column -->
+     <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+       <!-- Links -->
+       <h6 class="text-uppercase font-weight-bold">Contact</h6>
+       <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+       <p>
+         <i class="fas fa-home mr-3"></i>Prishtine, Kodi:10000, Kosove</p>
+       <p>
+         <i class="fas fa-envelope mr-3"></i> pik@gmail.com</p>
+       <p>
+         <i class="fas fa-phone mr-3"></i> + 383 44 841000</p>
+       <p>
+         <i class="fas fa-print mr-3"></i> + 383 44 249523</p>
+
+     </div>
+     <!-- Grid column -->
+
+   </div>
+   <!-- Grid row -->
+
+ </div>
+ <!-- Footer Links -->
+
+ <!-- Copyright -->
+ <div class="footer-copyright text-center py-3">© 2020 Copyright:
+   <a href="http://localhost:8080/shareposts"> Portali Informativ I Kosoves</a>
+ </div>
+ <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+<!-- <footer class="page-footer font-small mdb-color pt-4">
 <div class="container text-center text-md-left">
 
   <div class="row text-center text-md-left mt-3 pb-3">    
     <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
       <h6 class="text-uppercase mb-4 font-weight-bold">Portali Informativ i Kosoves</h6>
-      <p>Portali yne operon qe nga viti 2019. Ishte nje ide e rastesishme e cila pastaj u shenderrua ne nje kryeveper te madhe.</p>
+      <p>Portali yne operon qe nga viti 2019. Ishte nje ide e rastesishme e cila pastaj u shenderrua ne nje kryeveper te madhe!!!</p>
     </div>
   
     <hr class="w-100 clearfix d-md-none">
@@ -119,7 +236,7 @@
 
 </div>
 
-</footer>
+</footer> -->
 
 </body>
 </html>
